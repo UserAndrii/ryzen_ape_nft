f02a5190a3b63e4d6c538d87221841d815a4b937
Andrii Gadar <gadar.andre@gmail.com>
2024-03-25 02:28:08 +0200

    fixed the maximum value of adaptive layout

--- src/components/Arts/Arts.tsx
+++ src/components/Arts/Arts.tsx
@@ -10,32 +10,34 @@ const Arts: React.FC = () => {
     <section className={s.arts} id="arts">
       <h2 className={s.arts__title}>COLLECTION</h2>
 
-      <SliderWrapper>
-        {artsData.map(({ id, image, imageTab }) => (
-          <div key={id} className={s.arts__slide}>
-            <picture>
-              <source
-                type="image/png"
-                media="(min-width: 768px)"
-                srcSet={`${imageTab[0]} 1x, ${imageTab[1]} 2x`}
-              />
-              <source
-                type="image/png"
-                media="(max-width: 767px)"
-                srcSet={`${image[0]} 1x, ${image[1]} 2x`}
-              />
-              <img
-                src={`${image[0]}`}
-                alt="Ape collections"
-                width={216}
-                height={256}
-                className={s.arts__image}
-                loading="lazy"
-              />
-            </picture>
-          </div>
-        ))}
-      </SliderWrapper>
+      <div className={s.arts__slick}>
+        <SliderWrapper>
+          {artsData.map(({ id, image, imageTab }) => (
+            <div key={id} className={s.arts__slide}>
+              <picture>
+                <source
+                  type="image/png"
+                  media="(min-width: 768px)"
+                  srcSet={`${imageTab[0]} 1x, ${imageTab[1]} 2x`}
+                />
+                <source
+                  type="image/png"
+                  media="(max-width: 767px)"
+                  srcSet={`${image[0]} 1x, ${image[1]} 2x`}
+                />
+                <img
+                  src={`${image[0]}`}
+                  alt="Ape collections"
+                  width={216}
+                  height={256}
+                  className={s.arts__image}
+                  loading="lazy"
+                />
+              </picture>
+            </div>
+          ))}
+        </SliderWrapper>
+      </div>
     </section>
   );
 };
